3d9f043011cb1c387d335d4d178f077c4cec77d8
deakiniotlab <deol.satish@deakin.edu.au>
2025-01-06 13:13:39 +1100

    Changes to remove util module dependency

--- utils/exp_pool.py
+++ utils/exp_pool.py
@@ -1,4 +1,3 @@
-
 class ExperiencePool:
     """
     Experience pool for collecting trajectories.
@@ -10,7 +9,7 @@ class ExperiencePool:
         self.dones = []
 
     def add(self, state, action, reward, done):
-        self.states.append(state)  # sometime state is also called obs (observation)
+        self.states.append(state)  # Sometimes state is also called obs (observation)
         self.actions.append(action)
         self.rewards.append(reward)
         self.dones.append(done)
@@ -18,3 +17,23 @@ class ExperiencePool:
     def __len__(self):
         return len(self.states)
 
+    def __getstate__(self):
+        """
+        Custom method to serialize the state without relying on the class or module.
+        """
+        # Return only the necessary data for pickling
+        return {
+            'states': self.states,
+            'actions': self.actions,
+            'rewards': self.rewards,
+            'dones': self.dones,
+        }
+
+    def __setstate__(self, state):
+        """
+        Custom method to restore the state when unpickling, without requiring the class/module.
+        """
+        self.states = state['states']
+        self.actions = state['actions']
+        self.rewards = state['rewards']
+        self.dones = state['dones']
